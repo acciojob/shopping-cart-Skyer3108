14ca3057a77003c1c5750715f3d49017099ca5d6
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-07 14:52:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+const nam=document.getElementById('name')
+
+const pric=document.getElementById('price')
+
+const addbtn=document.getElementById('add')
+
+const tbody=document.getElementById('data')
+
+addbtn.addEventListener('click',()=>{
+	const name=nam.value
+	const price=pric.value
+
+	const tr=document.createElement('tr');
+	tr.innerHTML=`
+	<td>${name}</td>
+				<td>${price}</td>
+	`
+
+	tbody.appendChild(tr)
+
+	nam.value=''
+	pric.value=''
+	
+
+	
+	
+})
